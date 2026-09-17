b3296654084ba7879d1f27c00192d56c3df97ade
iartecheur26aabd <iartecheur26aabd@ikzubirimanteo.com>
2026-09-17 10:10:23 +0200

    ariketa_bukatuak

---
+++ Ariketa import/matematika.py
@@ -0,0 +1,11 @@
+def karratua(n):
+    emaitza=n*n
+    return emaitza
+
+
+
+def da_bikoitia(n):
+    if n%2==0:
+        return True
+    else:
+        return False
---
+++ Ariketa import/probak.py
@@ -0,0 +1,16 @@
+import matematika
+
+print(f"{matematika.karratua(5)}")
+print(f"{matematika.karratua(10)}")
+
+print(f"{matematika.da_bikoitia(12)}")
+print(f"{matematika.da_bikoitia(7)}")
+
+####################################
+from matematika import da_bikoitia,karratua
+
+print(f"{karratua(2)}")
+print(f"{karratua(14)}")
+
+print(f"{da_bikoitia(2)}")
+print(f"{da_bikoitia(9)}")
